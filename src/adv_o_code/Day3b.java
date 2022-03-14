e074531b10854f58cda559b9d04e8cec06c682cd
evang <evang@192.168.1.121>
2022-03-14 17:27:20 +0100

    Day3

--- src/adv_o_code/Day3b.java
+++ src/adv_o_code/Day3b.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Day3b {
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner input = new Scanner(new File("sample03.txt"));
+		Scanner input = new Scanner(new File("day03.txt"));
 
 		ArrayList<String> inA = new ArrayList<>();
 		ArrayList<String> inB = new ArrayList<>();
@@ -26,105 +26,94 @@ public class Day3b {
 
 		// inA is altered list with most common values
 
-		int zeroA = 0;
-		int oneA = 0;
+		int zero = 0;
+		int one = 0;
 
-		int lengthStrA = inA.get(0).length();
+		int lengthStr = inA.get(0).length();
 
-//		while (inA.size() > 1) {
+	//	while (inA.size() > 1) {
 			int counter = 0;
-		
-			System.out.println(inA);
-			for (int i = 0; i < inA.size(); i++) {
-				for (int j = counter; j < lengthStrA; j += lengthStrA) {
-					if (inA.get(i).charAt(j) == '0') {
-						zeroA++;
-					} else {
-						oneA++;
-					}
-
-				}
-			}
-			System.out.println(zeroA + " " + oneA);
-
-			for (int i = 0; i < inA.size(); i++) {
-				if (zeroA < oneA) {
-					if (inA.get(i).charAt(counter) == '0') {
-						inA.remove(i);
-						i--;
-					}
-				} else {
-					if (inA.get(i).charAt(counter) == '1') {
-						inA.remove(i);
-						i--;
-					}
-				}
-//				System.out.println(inA);
-			}
-			zeroA = 0;
-			oneA = 0;
-			counter++;
-			
-			
-			System.out.println(inA);
-			System.out.println(inA);
-			for (int i = 0; i < inA.size(); i++) {
-				for (int j = counter; j < lengthStrA; j += lengthStrA) {
-					if (inA.get(i).charAt(j) == '0') {
-						zeroA++;
-					} else {
-						oneA++;
-					}
-
-				}
-			}
-			System.out.println(zeroA + " " + oneA);
-
-			for (int i = 0; i < inA.size(); i++) {
-				if (zeroA < oneA) {
-					if (inA.get(i).charAt(counter) == '0') {
-						inA.remove(i);
-						i--;
-					}
-				} else {
-					if (inA.get(i).charAt(counter) == '1') {
-						inA.remove(i);
-						i--;
-					}
-				}
-				System.out.println(inA);
-			}
-//			for (int i = 0; i < inA.size(); i++) {
-//				for (int j = 1; j < lengthStrA; j += lengthStrA) {
-//					if (inA.get(i).charAt(j) == '0') {
-//						zeroA++;
-//					} else {
-//						oneA++;
-//					}
-//
-//				}
-//			}
-//			System.out.println(zeroA + " " + oneA);
 //			System.out.println(inA);
-//
-//			for (int i = 0; i < inA.size(); i++) {
-//				if (zeroA < oneA) {
-//					if (inA.get(i).charAt(1) == '0') {
-//						inA.remove(i);
-//						i--;
-//					}
-//				} else {
-//					if (inA.get(i).charAt(1) == '1') {
-//						inA.remove(i);
-//						i--;
-//					}
-//				}
-//				System.out.println(inA);
-//			}
-//			System.out.println(inA);
-//			zeroA = 0;
-//			oneA = 0;
-//		}
+			for (int n = 0; n < lengthStr; n++) {
+				for (int i = 0; i < inA.size(); i++) {
+					for (int j = counter; j < lengthStr; j += lengthStr) {
+						if (inA.get(i).charAt(j) == '0') {
+							zero++;
+						} else {
+							one++;
+						}
+
+					}
+				}
+//				System.out.println(zero + " " + one);
+
+				for (int i = 0; i < inA.size(); i++) {
+					if(inB.size() == 1) {
+						break;
+					}
+//					System.out.println(inA);
+					if (zero <= one) {
+						if (inA.get(i).charAt(counter) == '0') {
+							inA.remove(i);
+							i--;
+						}
+					} else {
+						if (inA.get(i).charAt(counter) == '1') {
+							inA.remove(i);
+							i--;
+						}
+					}
+				}
+	//			System.out.println(inA);
+				zero = 0;
+				one = 0;
+				counter++;
+			}
+			
+			counter = 0;
+//			System.out.println(inB);
+			for (int n = 0; n < lengthStr; n++) {
+				for (int i = 0; i < inB.size(); i++) {
+					for (int j = counter; j < lengthStr; j += lengthStr) {
+						if (inB.get(i).charAt(j) == '0') {
+							zero++;
+						} else {
+							one++;
+						}
+
+					}
+				}
+//				System.out.println(zero + " " + one);
+
+				for (int i = 0; i < inB.size(); i++) {
+					if(inB.size() == 1) {
+						break;
+					}
+//					System.out.println(inB);
+					if (zero > one) {
+						if (inB.get(i).charAt(counter) == '0') {
+							inB.remove(i);
+							i--;
+						}
+					} else {
+						if (inB.get(i).charAt(counter) == '1') {
+							inB.remove(i);
+							i--;
+						}
+					}
+				}
+//				System.out.println(inB);
+				zero = 0;
+				one = 0;
+				counter++;
+			}
+			System.out.println(inB + " " + inA);
+			int oxGen = Integer.parseInt(inA.get(0), 2);
+			int CO2 = Integer.parseInt(inB.get(0), 2);
+			
+			int res = oxGen * CO2;
+			
+			System.out.println(res);
+			
 	}
-
 }
